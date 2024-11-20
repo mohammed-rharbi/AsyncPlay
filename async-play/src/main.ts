5b43e75210ec381ea139485296b91bcd81d2579b
mohammed-rharbi <rharbi383@gmail.com>
2024-11-20 16:55:32 +0100

    build : install nodmiler

--- async-play/src/main.ts
+++ async-play/src/main.ts
@@ -1,8 +1,14 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { Logger } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  app.enableCors();
+
+
   await app.listen(process.env.PORT ?? 3000);
+  Logger.log(`this app is running at : http://localhost/${process.env.PORT ?? 3000}`,'Bootsrap')
 }
 bootstrap();
