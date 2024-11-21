0517309fa76f73ad9d4b25ced8771ca8cbd2df73
mohammed-rharbi <rharbi383@gmail.com>
2024-11-21 15:27:25 +0100

    build: create play list entity

--- async-play/src/main.ts
+++ async-play/src/main.ts
@@ -1,11 +1,13 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import { Logger } from '@nestjs/common';
+import { Logger, ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.enableCors();
+
+  app.useGlobalPipes(new ValidationPipe());
 
 
   await app.listen(process.env.PORT ?? 3000);
---
+++ async-play/src/playlist/playlist.entity.ts
@@ -0,0 +1,17 @@
+import { Schema ,Prop  ,SchemaFactory } from "@nestjs/mongoose";
+import { Document , Types } from "mongoose";
+
+@Schema()
+export class playlist extends Document{
+    
+    @Prop({required:true})
+    title: string;
+
+    @Prop({ type: Types.ObjectId , ref: 'User' , required:true})
+    onwer: Types.ObjectId;
+
+    @Prop({ type: [{ type: Types.ObjectId }] , ref: 'Video' })
+    viedos: Types.ObjectId[];
+}
+
+export const playlistShema = SchemaFactory.createForClass(playlist);
