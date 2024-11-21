0962358c05cc801092deedb8613e3c71c4f1488e
mohammed-rharbi <rharbi383@gmail.com>
2024-11-21 10:10:33 +0100

    feat: regester the user and playlist module

--- async-play/src/app.module.ts
+++ async-play/src/app.module.ts
@@ -4,13 +4,17 @@ import { AppService } from './app.service';
 import { ConfigModule } from '@nestjs/config';
 import { MongooseModule } from '@nestjs/mongoose';
 import { UserModule } from './user/user.module';
+import { PlaylistService } from './playlist/playlist.service';
+import { PlaylistController } from './playlist/playlist.controller';
+import { PlaylistModule } from './playlist/playlist.module';
 
 @Module({
   imports: [ ConfigModule.forRoot({isGlobal:true}) ,
              MongooseModule.forRoot(process.env.DB),
-             UserModule
+             UserModule,
+             PlaylistModule
   ],
-  controllers: [AppController],
-  providers: [AppService],
+  controllers: [AppController, PlaylistController],
+  providers: [AppService, PlaylistService],
 })
 export class AppModule {}
