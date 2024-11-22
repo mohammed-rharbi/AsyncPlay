f33909727f14d121b6b6328fac84b40035cb05cd
mohammed-rharbi <rharbi383@gmail.com>
2024-11-22 10:01:23 +0100

    feat: add create playlist method to playlist conroller

--- async-play/src/playlist/playlist.controller.ts
+++ async-play/src/playlist/playlist.controller.ts
@@ -1,4 +1,22 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Post } from '@nestjs/common';
+import { PlaylistService } from './playlist.service';
+import { createPlayListDTO } from './DTO/createPlaylist';
 
 @Controller('playlist')
-export class PlaylistController {}
+export class PlaylistController {
+
+    constructor(private readonly playlistService: PlaylistService){}
+
+
+    @Post('create/playlist')
+    async createPlaylist(@Body() playlistData: createPlayListDTO){
+
+        return await this.playlistService.createPlaylist(playlistData);
+
+    }
+
+ 
+    
+
+
+}
