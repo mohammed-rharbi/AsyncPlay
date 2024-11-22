a38ad03a220fc544ac95047a623cb0cd47f8c284
mohammed-rharbi <rharbi383@gmail.com>
2024-11-22 19:25:20 +0100

    chore: update Redux store configuration

--- frontend/src/app/store.ts
+++ frontend/src/app/store.ts
@@ -1,11 +1,10 @@
 import { configureStore } from "@reduxjs/toolkit";
-import counterReducer  from '../features/counterSlice';
-
+import playListReducer from "../features/playList/playListSlice"
 
 export const store = configureStore({
 
     reducer:{
-        counter: counterReducer 
+        playList: playListReducer 
 
     }
 
