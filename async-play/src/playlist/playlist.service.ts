43ff903bc7c5d3a35e70d924aec04d639a803d88
mohammed-rharbi <rharbi383@gmail.com>
2024-11-22 10:01:47 +0100

    feat: add create playlist method to playlist service

--- async-play/src/playlist/playlist.service.ts
+++ async-play/src/playlist/playlist.service.ts
@@ -1,4 +1,20 @@
 import { Injectable } from '@nestjs/common';
+import { playlistRepositorie } from './playlist.repository';
+import { playlist } from './playlist.entity';
+import { createPlayListDTO } from './DTO/createPlaylist';
 
 @Injectable()
-export class PlaylistService {}
+export class PlaylistService {
+
+    constructor(private readonly playlistRepositorie: playlistRepositorie){}
+
+
+    async createPlaylist(playlistData: createPlayListDTO): Promise<playlist>{
+
+
+      return await this.playlistRepositorie.createPlayList(playlistData);
+
+
+    }
+
+}
