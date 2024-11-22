08770d77286c5ededf8a7a238bedca483089294e
mohammed-rharbi <rharbi383@gmail.com>
2024-11-22 10:02:53 +0100

    feat: register playlist provider in playlist module

--- async-play/src/playlist/playlist.module.ts
+++ async-play/src/playlist/playlist.module.ts
@@ -1,4 +1,17 @@
 import { Module } from '@nestjs/common';
+import { PlaylistController } from './playlist.controller';
+import { PlaylistService } from './playlist.service';
+import { playlistRepositorie } from './playlist.repository';
+import { ConfigModule } from '@nestjs/config';
+import { MongooseModule } from '@nestjs/mongoose';
+import { playlist, playlistShema } from './playlist.entity';
 
-@Module({})
+@Module({
+
+    providers: [PlaylistService , playlistRepositorie] ,
+    controllers: [PlaylistController],
+    imports: [ ConfigModule , MongooseModule.forFeature([{name: playlist.name , schema: playlistShema}])]
+  
+
+})
 export class PlaylistModule {}
