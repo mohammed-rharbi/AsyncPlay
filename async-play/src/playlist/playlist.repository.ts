78150f83a9dfb8d913a9416bf9744d08d0d45f27
mohammed-rharbi <rharbi383@gmail.com>
2024-11-22 12:18:04 +0100

    feat: add getUserPlaylists method to get playlists

--- async-play/src/playlist/playlist.repository.ts
+++ async-play/src/playlist/playlist.repository.ts
@@ -1,6 +1,6 @@
 import { playlist , playlistShema } from "./playlist.entity";
 import { Model } from "mongoose";
-import { Injectable } from "@nestjs/common";
+import { Injectable, NotFoundException } from "@nestjs/common";
 import { InjectModel } from "@nestjs/mongoose";
 import { createPlayListDTO } from "./DTO/createPlaylist";
 
@@ -16,6 +16,17 @@ export class playlistRepositorie {
         const playlist = new this.playListModel(playlistData);
 
         return playlist.save();
+    }
+
+    async getUserPlaylist(userId: string): Promise<playlist[]>{
+
+        const playlists =  await this.playListModel.find({owner: userId}).exec();
+
+        if(!playlists){
+            throw new NotFoundException('no playlist found')
+        }
+
+        return playlists
     }
 
 }
