63ce6c999b2f52f663da49bb22027b19482f2072
mohammed-rharbi <rharbi383@gmail.com>
2024-11-22 09:45:53 +0100

    feat: create playlist DTO

---
+++ async-play/src/playlist/DTO/createPlaylist.ts
@@ -0,0 +1,18 @@
+import { IsNotEmpty , IsString , Length } from "class-validator";
+
+
+export class createPlayListDTO {
+
+    @IsString()
+    @IsNotEmpty()
+    @Length(3 , 20)
+    title: string;
+
+
+    @IsString()
+    @IsNotEmpty()
+    onwer: string;
+
+
+}
+
